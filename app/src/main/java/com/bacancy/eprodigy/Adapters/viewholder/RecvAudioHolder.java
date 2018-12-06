7dd2cab79c3adf034fc5a5ab146119d692bfc52c
Mohit Kanada <mohit.kanada@bacancytechnology.com>
2018-12-06 19:18:20 +0530

    Share audio files

---
+++ app/src/main/java/com/bacancy/eprodigy/Adapters/viewholder/RecvAudioHolder.java
@@ -0,0 +1,11 @@
+package com.bacancy.eprodigy.Adapters.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class RecvAudioHolder extends RecyclerView.ViewHolder{
+
+    public RecvAudioHolder(View itemView) {
+        super(itemView);
+    }
+}
---
+++ app/src/main/java/com/bacancy/eprodigy/Adapters/viewholder/SendAudioHolder.java
@@ -0,0 +1,11 @@
+package com.bacancy.eprodigy.Adapters.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class SendAudioHolder extends RecyclerView.ViewHolder{
+
+    public SendAudioHolder(View itemView) {
+        super(itemView);
+    }
+}
